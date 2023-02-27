6a069a2f5b0f3d01dcde2b2fc67b146cd2ece85d
Jack <56638954+JackThay@users.noreply.github.com>
2023-02-27 17:26:17 +0100

    Nettoyage

--- AIMD/out/clang-release/receiver.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//receiver.o: receiver.cc receiver.h \
-  D:/omnetpp-6.0.1/include/omnetpp.h
-
-receiver.h:
-
-D:/omnetpp-6.0.1/include/omnetpp.h:
--- AIMD/out/clang-release/router.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//router.o: router.cc router.h \
-  D:/omnetpp-6.0.1/include/omnetpp.h
-
-router.h:
-
-D:/omnetpp-6.0.1/include/omnetpp.h:
