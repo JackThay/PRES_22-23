2889e4d7ef1ce379223e0a06717086cf93128630
Dorian95870 <79307259+Dorian95870@users.noreply.github.com>
2023-05-07 21:07:57 +0200

    algoritm rto

--- algo_coap_5v5/shared_function.cc
+++ algo_coap_5v5/shared_function.cc
@@ -1,5 +1,7 @@
 #include "shared_function.h"
 
+//using namespace std;
+
 double randomDouble(double lowerLimit, double upperLimit)
 {
     std::random_device rd;
@@ -17,3 +19,62 @@ int getDownloadSpeed(double rtt, int packet_size)
     int result = int(speed);
     return result;
 }
+
+double calculateRTO(double rtt, bool isSpurious) {
+    double alpha = 0.125;
+    double gamma = 0.03125;
+    double SRTT = rtt;
+    int K = 4;
+    double RTTVAR = rtt / 2;
+    double RTO;
+
+    // update SRTT
+    SRTT = (1 - alpha) * SRTT + alpha * rtt;
+
+    // update RTTVAR
+    if (rtt > SRTT) {
+        RTTVAR = (1 - alpha) * RTTVAR + alpha * abs(SRTT - rtt);
+    }
+    else {
+        RTTVAR = (1 - gamma) * RTTVAR + gamma * abs(SRTT - rtt);
+    }
+
+    // update K
+    if (isSpurious == true) {
+        K = 7;
+    }
+    else {
+        K = 4;
+    }
+
+    // calculate RTO
+    RTO = SRTT + K * RTTVAR;
+    return RTO;
+}
+
+/*
+int main() {
+    double rtt = 100; // initial RTT value
+    bool isSpurious = false; // initial value of spurious status
+
+    // measure new RTT values and calculate RTO
+    //for (int i = 0; i < 1000; i++) {
+        double newrtt = // measure new RTT value
+        RTO = calculateRTO(newrtt,isSpurious);
+
+        // check if transmission is spurious
+        if (newrtt > RTO) {
+            isSpurious = true;
+        }
+        else {
+            isSpurious = false;
+        }
+
+        // update R and print RTO
+        rtt = newrtt;
+        cout << "RTO: " << RTO <<"\n";
+    //}
+
+    return 0;
+}
+*/
--- algo_coap_5v5/shared_function.h
+++ algo_coap_5v5/shared_function.h
@@ -4,8 +4,10 @@
 #include <string.h> // needed for string
 #include <cstdlib> // needed to generate random number
 #include <iostream>
+#include <cmath>
 #include <time.h>
 #include <random>
 
 double randomDouble(double lowerLimit, double upperLimit);
 int getDownloadSpeed(double rtt, int size);
+double calculateRTO(double rtt, bool isSpurious);
